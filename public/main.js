908e8c30d94f86cd2351cb48f22b939e5dfd74b3
Joshua Santos <90679399+Joshuaxsl@users.noreply.github.com>
2023-02-23 21:14:43 +0800

    practice jquery

--- public/main.js
+++ public/main.js
@@ -20,6 +20,62 @@ function changeBgColor(){
 
 
 
-function numAdd(){
-    
+function numAdd(num1, num2){
+
 }
+
+$("p").hide()
+
+
+$("#show").click(
+    function(){
+    $("p").show();
+  }
+);
+
+
+
+$(function(){
+    $("#hide").click(function(){
+      $("p").hide();
+    });
+    $("#show").click(function(){
+      $("p").show();
+    });
+  });
+
+  $(function(){
+    $("#hide").click(function(){
+      $("p").hide();
+    });
+    $("#show").click(function(){
+      $("p").show();
+    });
+  });
+
+//   $(function(){
+//     $("#myBtn1").click(function(){
+//         $("#name").hide(1000);
+//     });
+
+//     $("#myBtn2").click(function(){
+//         $("#name").show(1000);
+//     }); 
+//   });
+  $(function(){
+    $("#myBtn1").click(function(){
+        $("#name").toggle();
+    });
+  });
+
+  
+ 
+ 
+ 
+ 
+ 
+//   $(function(){
+//     $("myBtn2").click(function(){
+//         $("#name").show(1000);
+//     });
+//   });
