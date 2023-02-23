11d8b05989cbfb5b195c5e90f59e0169a5f7c6ce
Joshua Santos <90679399+Joshuaxsl@users.noreply.github.com>
2023-02-23 16:17:58 +0800

    change directory

---
+++ public/main.js
@@ -0,0 +1,25 @@
+function myFunction(a, b){
+    sum = a +b ;
+    return sum;
+}
+ 
+document.getElementById('section-hero').innerHTML = myFunction(3, 10);
+
+function myBtn(){
+    document.getElementById('spanBtn').innerHTML = "Joshua Pogi"
+}
+
+
+
+
+
+
+function changeBgColor(){
+    document.getElementById('section-hero').style.backgroundColor = "red"
+}
+
+
+
+function numAdd(){
+    
+}
