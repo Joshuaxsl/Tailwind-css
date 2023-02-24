eab318cb407730b5287fb95a728abb04adb65412
Joshua Santos <90679399+Joshuaxsl@users.noreply.github.com>
2023-02-24 18:37:51 +0800

    added comment

--- public/main.js
+++ public/main.js
@@ -10,23 +10,9 @@ function myBtn(){
 }
 
 
-
-
-
-
-function changeBgColor(){
-    document.getElementById('section-hero').style.backgroundColor = "red"
-}
-
-
-
-function numAdd(num1, num2){
-
-}
-
 $("p").hide()
 
-
+//show
 $("#show").click(
     function(){
     $("p").show();
@@ -34,7 +20,7 @@ $("#show").click(
 );
 
 
-
+//Hide and show the element
 $(function(){
     $("#hide").click(function(){
       $("p").hide();
@@ -53,15 +39,8 @@ $(function(){
     });
   });
 
-//   $(function(){
-//     $("#myBtn1").click(function(){
-//         $("#name").hide(1000);
-//     });
 
-//     $("#myBtn2").click(function(){
-//         $("#name").show(1000);
-//     }); 
-//   });
+//jquery toggler(hide and show)
   $(function(){
     $("#myBtn1").click(function(){
         $("#name").toggle();
@@ -75,7 +54,7 @@ $(function(){
  const firstName = 'Joshua'
  console.log(firstName)
 
-
+//Function and statement practice
  function myAge(age){
   if(age < 18){
     return "Minor";
@@ -87,25 +66,21 @@ $(function(){
  }
   console.log(myAge(15))
 
+
+  //Looking up for data types
   const lastName = "Santos"
   let currentAge = 22;
-
   console.log(typeof currentAge);
   console.log(typeof lastName);
 
-
- const joshuaSantos = [
-  
- ]
-
-
+//Contatenation with using the symbol(``)
 const myName = 'Joshua Santos';
 const currAge = ' 22 Years Old Student';
 const myLocation = 'Pampanga';
 let myInfo = `Hello Im ${myName} a ${currAge} from ${myLocation}`;
 console.log(myInfo);
 
-
+//Array
 const myHobby = [
   'Basketball','Movies','Program'
 ];
@@ -116,7 +91,7 @@ console.log(Array.isArray(myHobby));
 console.log(myHobby.indexOf("Swimming"))
  
 
-
+//javascript objects
 const techTools = {
     laptop: "Acer Nitro",
     mouse: "A4tech",
@@ -130,7 +105,7 @@ const techTools = {
 console.log(techTools.laptopSpecs)
 
 
-
+//array and inside objects
 const techStacks = [
   {
     id:1,
@@ -150,14 +125,18 @@ const techStacks = [
 ]
 console.log(techStacks[2].experience);
 
+
+//for loop
 for(let x = 0; x < techStacks.length  ; x++){
     console.log(techStacks[x].proglang);
 }
 
+//easiest way of creating for loop
 for(let stack of techStacks){
   console.log(stack.experience)
 };
 
+//filter function
 const myTechStacks = techStacks.filter(function(myStacks){
 
   return myStacks.id == 1;
@@ -166,12 +145,12 @@ const myTechStacks = techStacks.filter(function(myStacks){
 console.log(myTechStacks);
 
 
-
+//JSON 
 const techStacksJSON = JSON.stringify(techStacks);
 console.log(techStacksJSON);
 
 
-
+//if statement
 let joshAge = 22;
 if(joshAge = 22){
   console.log('True');
@@ -179,6 +158,7 @@ if(joshAge = 22){
   console.log("false");
 }
 
+//If statement practice
 function joshFunction(year){
   if(year === 1){
     return "You're a First Year College";
@@ -197,6 +177,8 @@ function joshFunction(year){
 }
 console.log(joshFunction(1));
 
+
+//Other syntax for function
 const modernFunction = (num1, num2) => {
   return num1 * num2;
 }
